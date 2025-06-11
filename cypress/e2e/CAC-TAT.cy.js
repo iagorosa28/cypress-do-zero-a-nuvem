4d05872ed4f5c09e1d2e9ffdaf6f0840a32bc47f
Iago Rosa <iagoimportantthings@gmail.com>
2025-06-11 20:25:59 -0300

    Primeiro teste

--- cypress/e2e/CAC-TAT.cy.js
+++ cypress/e2e/CAC-TAT.cy.js
@@ -1,5 +1,11 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
+// describe -> switch de testes
+// it -> caso de teste
+
+describe('Central de Atendimento ao Cliente TAT', () => {
+  it('verifica o título da aplicação', () => {
+    cy.visit('./src/index.html')
+    //cy.title().should('eq', 'Central de Atendimento ao Cliente TAT')
+    cy.title().should('be.equal', 'Central de Atendimento ao Cliente TAT') // da aula kkkk mas o de cima está certo tbm
+    // da para usar 'not.be.equal' se quiser pegar algo que não for igual 
   })
 })
